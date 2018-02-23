4302ff784fb29bf688615dbd7546c125d7740274
yeahlam <tedy@live.cn>
2018-02-23 15:44:29 +0800

    完成头部组件基础布局

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,25 +1,67 @@
 <template>
-  <div class="header">
-      <div class="">{{poiinfo.name}}</div>
-      <span class="icon-arrow_lift"></span>
-  </div>
+	<div class="header">
+		<div class="wraper">
+			<div class="top-wrapper">
+				<div class="leftbtn-wrapper">
+					<span class="icon-arrow_lift"></span>
+				</div>
+				<form class="search-wrapper">
+					<div class="searchlogo"></div>
+					<input type="text" />
+				</form>
+				<div class="rightbtn-wrapper">
+					<span class="pdbtn">拼单</span>
+					<span class="morebtn">
+						<i></i>
+						<i></i>
+						<i></i>
+					</span>
+				</div>
+			</div>
+			<div class="mid-wrapper">
+				<div class="showicon">
+					<img :src="poiinfo.pic_url"/>
+				</div>
+				<div class="showname">{{poiinfo.name}}</div>
+				<div class="fav" @click="favtap">
+					<div class="favlogo"><span class="icon-favorite"></span></div>
+					<div class="favfont">收藏</div>
+				</div>
+			</div>
+			<div class="bottom-wrapper">
+				<div class="logo">
+					<img v-if="poiinfo.discounts2" :src="poiinfo.discounts2[0].icon_url"/>
+				</div>
+				<div v-if="poiinfo.discounts2" class="detail">{{poiinfo.discounts2[0].info}}</div>
+				<div v-if="poiinfo.discounts2" class="activenum">{{poiinfo.discounts2.length}}个活动<span class="icon-keyboard_arrow_right"></span></div>
+			</div>
+			<div class="bg-wrapper">
+				<img :src="poiinfo.head_pic_url"/>
+			</div>
+		</div>
+
+	</div>
 </template>
 
 <script>
-
-
-export default {
-  props:{
-    poiinfo:{
-      type:Object,
-      defaule:{}
-    }
-  }
-}
+	export default {
+		props: {
+			poiinfo: {
+				type: Object,
+				defaule: {}
+			}
+		},
+		methods:{
+			favtap:function(){
+				alert(12)
+			}
+		}
+	}
 </script>
 
-<style>
+<style lang="less">
 	/*加载图标字体*/
-@import url("../../common/css/style.css");
-
+	
+	@import url("../../common/css/style.css");
+	@import url("./header.less");
 </style>
--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,24 +1,47 @@
 <template>
-  <div class="nav">
-      <router-link to='/goods' class="goods nav-item">点菜</router-link>
-      <router-link to='/ratings' class="ratings nav-item">评价</router-link>
-      <router-link to='/seller' class="seller nav-item">商家</router-link>
-  </div>
+	<div class="nav">
+		<router-link to='/goods' class="goods nav-item">点菜<i></i></router-link>
+		<router-link to='/ratings' class="ratings nav-item">评价<i></i></router-link>
+		<router-link to='/seller' class="seller nav-item">商家<i></i></router-link>
+	</div>
 </template>
 
 <script>
-export default {};
+	export default {};
 </script>
 
-<style>
-.nav {
-  height: 40px;
-  line-height: 40px;
-  text-align: center;
-  display: flex;
-}
-.nav-item {
-  flex: 1;
-  font-size: 13px;
-}
+<style lang="less">
+	.nav {
+		height: 80px;
+		line-height: 80px;
+		text-align: center;
+		display: flex;
+		width: 100%;
+		border-bottom:1px solid #e4e4e4;
+	}
+	
+	.nav-item {
+		flex: 1;
+		font-size: 26px;
+		color: #666666;
+		text-decoration: none;
+		position: relative;
+		i {
+			position: absolute;
+			width: 40px;
+			height: 3px;
+			bottom: 0;
+			left: 50%;
+			margin-left: -20px;
+			display: none;
+		}
+	}
+	
+	.nav-item.router-link-active {
+		color: #ffbb22;
+		i {
+			background: #ffbb22;
+			display: block;
+		}
+	}
 </style>
